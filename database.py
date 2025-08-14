e247c6715a32006d3eefe2b2c394cf59baefa3b3
Youssef pplo <youssefdev74@gmail.com>
2025-08-14 21:16:15 +0300

    Update database.py

--- database.py
+++ database.py
@@ -5,16 +5,15 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
 # Get Turso credentials from environment variables
-db_url = "libsql://easybio-vercel-icfg-763wtqln43yqganotgaqmrpf.aws-us-east-1.turso.io"
-auth_token = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NTUxOTQ1MzQsImlkIjoiODdhMjhmYTItNWE5ZC00MmE3LTg3N2ItYzU1M2M5MzRjZmE1IiwicmlkIjoiMWI4MDA1ZWEtNzMzNi00MjczLWIzYTMtZDE1ZmZhMTdlNTE0In0.vZiUrhjGV7X_4YCDzKlFFGoQCcd4W-x6pMv9V6wDBuTfldK_ERkesNmI0DXbKN6C36ueTwhiTqzH69GJI6rNCw"
+db_url = os.environ.get("TURSO_DATABASE_URL")
+auth_token = os.environ.get("TURSO_AUTH_TOKEN")
 
-# Create the full connection string for Turso
-# The 'libsql' scheme tells SQLAlchemy to use the Turso driver
-SQLALCHEMY_DATABASE_URL = f"sqlite+{db_url}/?authToken={auth_token}&secure=true"
+# This is the corrected connection string for the Turso/Hrana dialect
+SQLALCHEMY_DATABASE_URL = f"sqlite+hrana://{db_url}?authToken={auth_token}"
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False},
-    future=True
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
