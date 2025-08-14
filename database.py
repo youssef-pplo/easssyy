7e932d18934c6a07821c2aa9a9c35c370226d1c9
Youssef pplo <youssefdev74@gmail.com>
2025-08-14 21:18:52 +0300

    Update database.py

--- database.py
+++ database.py
@@ -8,8 +8,8 @@ import os
 db_url = os.environ.get("TURSO_DATABASE_URL")
 auth_token = os.environ.get("TURSO_AUTH_TOKEN")
 
-# This is the corrected connection string for the Turso/Hrana dialect
-SQLALCHEMY_DATABASE_URL = f"sqlite+hrana://{db_url}?authToken={auth_token}"
+# This is the corrected connection string using the 'libsql' dialect
+SQLALCHEMY_DATABASE_URL = f"sqlite+libsql://{db_url}?authToken={auth_token}&secure=true"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
