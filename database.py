5d5087efaed6b8f136d080991d2cf49b4797a13f
Youssef pplo <youssefdev74@gmail.com>
2025-08-14 22:54:47 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,18 +1,31 @@
 # database.py
 import os
 import motor.motor_asyncio
-# database.py
 from dotenv import load_dotenv
 
-load_dotenv() # This line reads your .env file
+load_dotenv()
 
 MONGO_URI = os.environ.get("MONGODB_URI")
 
-# Create an async client to connect to your MongoDB Atlas cluster
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+class DataBase:
+    client: motor.motor_asyncio.AsyncIOMotorClient = None
 
-# Get a reference to your database (it will be created if it doesn't exist)
-db = client.easybio_db
+db = DataBase()
 
-# Get a reference to your collection of students (like a table in SQL)
-student_collection = db.get_collection("students")
+async def get_database() -> motor.motor_asyncio.AsyncIOMotorDatabase:
+    return db.client.easybio_db
+
+async def connect_to_mongo():
+    print("Connecting to MongoDB...")
+    db.client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+    print("Connection successful.")
+
+async def close_mongo_connection():
+    print("Closing MongoDB connection...")
+    db.client.close()
+    print("Connection closed.")
+
+# We get the collection from the database now
+async def get_student_collection():
+    database = await get_database()
+    return database.get_collection("students")
