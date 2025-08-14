a6cfb1743f7ac37ae3a28e40b6eb4f3c3cb25506
Youssef pplo <youssefdev74@gmail.com>
2025-08-14 21:10:13 +0300

    Update database.py

--- database.py
+++ database.py
@@ -11,7 +11,6 @@ auth_token = os.environ.get("TURSO_AUTH_TOKEN")
 # Create the full connection string for Turso
 # The 'libsql' scheme tells SQLAlchemy to use the Turso driver
 SQLALCHEMY_DATABASE_URL = f"sqlite+{db_url}/?authToken={auth_token}&secure=true"
-
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False},
