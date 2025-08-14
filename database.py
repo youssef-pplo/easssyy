0b9114f7c9500a37b8df32e10f84b5aa662eb379
Youssef pplo <youssefdev74@gmail.com>
2025-08-14 21:10:55 +0300

    Update database.py

--- database.py
+++ database.py
@@ -5,8 +5,8 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
 # Get Turso credentials from environment variables
-db_url = os.environ.get("TURSO_DATABASE_URL")
-auth_token = os.environ.get("TURSO_AUTH_TOKEN")
+db_url = "libsql://easybio-vercel-icfg-763wtqln43yqganotgaqmrpf.aws-us-east-1.turso.io"
+auth_token = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NTUxOTQ1MzQsImlkIjoiODdhMjhmYTItNWE5ZC00MmE3LTg3N2ItYzU1M2M5MzRjZmE1IiwicmlkIjoiMWI4MDA1ZWEtNzMzNi00MjczLWIzYTMtZDE1ZmZhMTdlNTE0In0.vZiUrhjGV7X_4YCDzKlFFGoQCcd4W-x6pMv9V6wDBuTfldK_ERkesNmI0DXbKN6C36ueTwhiTqzH69GJI6rNCw"
 
 # Create the full connection string for Turso
 # The 'libsql' scheme tells SQLAlchemy to use the Turso driver
